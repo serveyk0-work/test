c2200a2529e7c494c8b135b64b14508b071f760e
Sergey Solovey <sergey.solovey@cravter.com>
2022-03-30 11:37:42 +0300

    add full home design

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,10 +1,33 @@
-import React, { FC } from 'react';
+import React, { useMemo } from 'react';
 import styles from './Home.module.scss';
+import { useSelector } from 'react-redux';
+import Tabs from '../../components/Tabs/Tabs';
+import { RootState } from '../../redux/store';
+import Input from '../../components/UI/Input/Input';
+import { useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
-const Home: FC = (): JSX.Element => {
+interface IFormData {
+  convert: number
+}
+
+const Home = () => {
+  const {id} = useParams();
+  const {register} = useForm<IFormData>();
+  const data = useSelector((state: RootState) => state?.apiReducer);
+
+  const value = useMemo(() => {
+    const {currentValue, options} = data;
+    if (currentValue && id) {
+      return (parseFloat(currentValue) * options[id]).toFixed(2);
+    }
+    return 0;
+  }, [data, id])
+
   return (
     <div className={styles.home}>
-      Home
+      <Tabs/>
+      <Input name={'convert'} register={register} type={'number'} controlledValue={value} />
     </div>
   );
 };
