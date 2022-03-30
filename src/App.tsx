7280143caa8c20e08d658ebd6245710b0c0efe68
Sergey Solovey <sergey.solovey@cravter.com>
2022-03-30 11:27:22 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './containers/Home/Home';
 
-const App = () => {
+const App: FC = (): JSX.Element => {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/:id" element={<Home/>}/>
+        <Route path="*" element={<h1>NOT FOUND PAGE</h1>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/containers/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import styles from './Home.module.scss';
+
+const Home: FC = (): JSX.Element => {
+  return (
+    <div className={styles.home}>
+      Home
+    </div>
+  );
+};
+
+export default Home;
